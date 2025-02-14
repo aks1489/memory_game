66a8d6f8e743f69a80312d0638fbd1ccc798ec73
Akash <akash.sarkar1489@outlook.com>
2025-02-14 10:01:05 +0530

    Interface

--- src/Interface.ts
+++ src/Interface.ts
@@ -1,5 +1,17 @@
-import { MouseEventHandler } from "react";
+import { BaseSyntheticEvent, MouseEventHandler } from "react";
 
 export interface GameCardProps {
     handelemojiClick: MouseEventHandler
 }
+
+export interface GameWrapperButton {
+    children: string,
+    handleClick: MouseEventHandler
+}
+export interface GameWrapper {
+    handleSubmit: MouseEventHandler
+}
+
+export type gameOnOff = boolean
+
+export type Ie = BaseSyntheticEvent
