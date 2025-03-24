760c7022f0ebf646aa20e25b6f0f865d7020639c
akash <akash.sarkar1489@outlook.com>
2025-03-24 12:10:59 +0530

    feat: enhance GameOver component with animated completion message and update EmojiButton styles

--- src/components/EmojiButton.tsx
+++ src/components/EmojiButton.tsx
@@ -1,10 +1,11 @@
 export default function EmojiButton({content, handleClick, selectedCard, matchedCard}: any) {
-    const cardStyle = selectedCard ? "btn-warning" : matchedCard ? "btn-success border border-white" : "btn-light"
+    const cardStyle =  matchedCard ? "btn-success border border-white disabled animation block_pointer" : selectedCard ? "btn-warning animation" : "btn-light"
     const buttonContent = selectedCard || matchedCard ? content : "❓"
     return(
         <button 
             type="button"
             onClick={handleClick}
+            disabled={matchedCard}
             className={`btn ${cardStyle} border-3 emo`}>
                 {buttonContent}
         </button>
