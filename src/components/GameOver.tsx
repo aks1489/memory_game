75f71d1eaa63bb3470b3941f6c82da960ca5ffb1
akash <akash.sarkar1489@outlook.com>
2025-03-24 11:33:49 +0530

    feat: enhance GameOver component with focus management and improved styling

--- src/components/GameOver.tsx
+++ src/components/GameOver.tsx
@@ -1,14 +1,22 @@
+import { useEffect, useRef } from "react"
+
 export default function GameOver() {
+    const divref = useRef<null | HTMLDivElement>(null)
+
+    useEffect(() => {
+        if(divref.current) {
+            divref.current.focus()
+        }
+    },[])
     return(
-        <div className="container">
+        <div className="container bg-success flex-column d-flex align-items-center p-2 rounded mb-2" ref={divref} tabIndex={-1}>
             <div className="row justify-content-center mt-5">
-                <div className="col-12 col-md-6 text-center">
-                    <h1 className="text-danger">You've matched all the memory cards!</h1>
-                    <p className="text-muted">Better luck next time!</p>
+                <div className="col text-center">
+                    <h1 className="text-white">You've matched all the memory cards!🎉</h1>
                 </div>
             </div>
             <div className="row justify-content-center mt-3">
-                <div className="col-12 col-md-6 text-center">
+                <div className="col text-center">
                     <button className="btn btn-primary" onClick={() => window.location.reload()}>Play Again</button>
                 </div>
             </div>
