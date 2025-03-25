12a1330a1dcefcdc6baac54c3e298ed5cbd75cb9
akash <akash.sarkar1489@outlook.com>
2025-03-25 18:31:02 +0530

    feat: enhance EmojiButton component with accessibility attributes and improved button content display

--- src/components/EmojiButton.tsx
+++ src/components/EmojiButton.tsx
@@ -1,12 +1,18 @@
-export default function EmojiButton({content, handleClick, selectedCard, matchedCard}: any) {
+import { decodeEntity } from 'html-entities'
+
+export default function EmojiButton({content, handleClick, selectedCard, matchedCard, index}: any) {
     const cardStyle =  matchedCard ? "btn-success border border-white disabled animation block_pointer" : selectedCard ? "btn-warning animation" : "btn-light"
-    const buttonContent = selectedCard || matchedCard ? content : "❓"
+    const buttonContent = selectedCard || matchedCard ? decodeEntity(content.htmlCode[0]) : "❓"
+
+    const btnArea = matchedCard ? `${decodeEntity(content.name)} Matched.` : selectedCard ? `${decodeEntity(content.name)} Selected - not matched yet.` : "Card Upside down."
     return(
         <button 
             type="button"
             onClick={selectedCard ? null : handleClick}
             disabled={matchedCard}
-            className={`btn ${cardStyle} border-3 emo`}>
+            className={`btn ${cardStyle} border-3 emo`}
+            arial-label={`Position ${index + 1}: ${btnArea}`}
+            aria-live='polite'>
                 {buttonContent}
         </button>
     )
