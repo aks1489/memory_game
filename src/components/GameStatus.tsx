b1910c2f9781a872c474be2f8aebae1c97288b4c
akash <akash.sarkar1489@outlook.com>
2025-03-25 19:15:21 +0530

    feat: add GameStatus component to display matched pairs and remaining cards

---
+++ src/components/GameStatus.tsx
@@ -0,0 +1,8 @@
+export default function GameStatus() {
+    return (
+        <div className="container bg-primary flex-row d-flex align-items-center justify-content-around py-2 rounded mb-3">
+            <p className="m-0">Matched Pairs:4</p>
+            <p className="m-0">Left to Match:5</p>
+        </div>
+    )
+}
